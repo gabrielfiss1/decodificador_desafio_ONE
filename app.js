245e28d520b97b6493c57d79eedf0550536d65ea
gabriel <gabriel--fiss@hotmail.com>
2024-07-03 15:42:59 -0300

    feat: func descriptografar

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function validaCampo(){
-    let palavra = document.querySelector('input').value;
+    let palavra = document.querySelector('textarea').value;
     palavra.toLowerCase();
     return console.log(palavra);
 }
 
 function criptografar(){
-    let palavra = document.querySelector('input').value;
+    let palavra = document.querySelector('textarea').value;
     let criptoE = 'enter';
     let criptoI = 'imes';
     let criptoA = 'ai';
@@ -18,11 +18,28 @@ function criptografar(){
         textoCriptografado = textoCriptografado.replaceAll('o', criptoO);
         textoCriptografado = textoCriptografado.replaceAll('u', criptoU);
 
+        palavra = document.querySelector('textarea').value = '';
     let campo = document.getElementById('resultadoTexto');
     campo.innerHTML = textoCriptografado;
-        //eturn console.log(textoCriptografado);
+        //return console.log(textoCriptografado);
 }
 
 function descriptografar(){
-    
+    //let campo = document.getElementById('resultadoTexto').innerHTML;
+    let palavra = document.querySelector('textarea').value;
+    let DesCriptoE = 'e';
+    let DesCriptoI = 'i';
+    let DesCriptoA = 'a';
+    let DesCriptoO = 'o';
+    let DesCriptoU = 'u';    
+
+    let textoDescriptografado = palavra.replaceAll('enter', DesCriptoE);
+        textoDescriptografado = textoDescriptografado.replaceAll('imes', DesCriptoI);
+        textoDescriptografado = textoDescriptografado.replaceAll('ai', DesCriptoA);
+        textoDescriptografado = textoDescriptografado.replaceAll('ober', DesCriptoO);
+        textoDescriptografado = textoDescriptografado.replaceAll('ufat', DesCriptoU);
+
+        palavra = document.querySelector('textarea').value = '';
+        let campo = document.getElementById('resultadoTexto');
+        campo.innerHTML = textoDescriptografado;
 }
